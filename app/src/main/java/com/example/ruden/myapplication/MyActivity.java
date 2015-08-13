2b35060ca262bc6b7897e42acca93156cdafb73e
yurir777 <yurir@rogers.com>
2015-08-13 13:23:04 -0400

    Changed contact data retrieval to async using loaders.

--- app/src/main/java/com/example/ruden/myapplication/MyActivity.java
+++ app/src/main/java/com/example/ruden/myapplication/MyActivity.java
@@ -1,10 +1,13 @@
 package com.example.ruden.myapplication;
 
 import android.app.ActionBar;
-import android.content.ContentResolver;
-import android.content.Intent;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.LoaderManager;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
@@ -22,10 +25,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MyActivity extends Activity {
+import java.util.HashMap;
+
+public class MyActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
     public final static String EXTRA_MESSAGE = "com.example.ruden.myapplication.MESSAGE";
     public final static String SAVED_MESSAGE_KEY = "com.example.ruden.myapplication.SAVED_MESSAGE";
-    public static final int PICK_CONTACT_REQUEST = 1;  // The request code
+
+    private final static int PICK_CONTACT_REQUEST = 1;  // The request code
+    private final static int CONTACT_LOADER_ID = 0;
+    private final static int PHONE_LOADER_ID = 1;
+    private final static int EMAIL_LOADER_ID = 2;
+    private final static String CONTACT_URI_KEY = "contactUri";
+    private final static String NAME_KEY = "name";
+    private final static String PHONE_KEY = "phone";
+    private final static String EMAIL_KEY = "email";
+
+    private Boolean contactLoaded = false;
+    private Boolean phoneLoaded = false;
+    private Boolean emailLoaded = false;
+
+    private HashMap<String, String> contactData = new HashMap<>(3);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -122,43 +141,104 @@ public class MyActivity extends Activity {
         if (requestCode == PICK_CONTACT_REQUEST) {
             // Make sure the request was successful
             if (resultCode == RESULT_OK) {
-                Uri contactUri = data.getData(); // has the uri for picked contact
-                String id = contactUri.getLastPathSegment();
-                ContentResolver resolver = getContentResolver();
-                String name = "", phone = "", email = "";
-
-                Cursor contactCursor = resolver.query(contactUri, null, null, null, null);
-                if (contactCursor.moveToFirst()) {
-                    name = contactCursor.getString(contactCursor.getColumnIndex(Contacts.DISPLAY_NAME));
-                }
-
-                Cursor phoneCursor = resolver.query(Phone.CONTENT_URI, null, Phone.CONTACT_ID + "=?", new String[] { id }, null);
-                if (phoneCursor.moveToFirst()) {
-                    phone = phoneCursor.getString(phoneCursor.getColumnIndex(Phone.NUMBER));
-                }
-
-                Cursor emailCursor = resolver.query(Email.CONTENT_URI, null, Email.CONTACT_ID + "=?", new String[]{id}, null);
-                if (emailCursor.moveToFirst()) {
-                    email = emailCursor.getString(emailCursor.getColumnIndex(Email.ADDRESS));
-                }
-
-                String message = "name: " + name + "\r\nemail: " + email + "\r\nphone: " + phone;
-                new AlertDialog.Builder(this)
-                        .setTitle(name)
-                        .setMessage(message)
-                        .setPositiveButton("ok", null)
-                        .show();
-
-                final Toast notifier = Toast.makeText(this, name + ", email: " + email + ", phone: " + phone, Toast.LENGTH_LONG);
-                notifier.show();
-                new CountDownTimer(9000, 1000)
-                {
-                    public void onTick(long millisUntilFinished) {notifier.show();}
-                    public void onFinish() {notifier.show();
-                    }
-                }.start();
+                contactLoaded = phoneLoaded = emailLoaded = false;
+                contactData.clear();
+                Uri contactUri = data.getData(); // Uri for picked contact
+                Bundle args = new Bundle(1);
+                args.putParcelable(CONTACT_URI_KEY, contactUri);
+                LoaderManager loader = getLoaderManager();
+                // Retrieve contact data async
+                loader.restartLoader(CONTACT_LOADER_ID, args, this);
+                loader.restartLoader(PHONE_LOADER_ID, args, this);
+                loader.restartLoader(EMAIL_LOADER_ID, args, this);
             }
         }
+    }
+
+    private void NotifyUser() {
+        String name = contactData.get(NAME_KEY);
+        String email = contactData.get(EMAIL_KEY);
+        String phone = contactData.get(PHONE_KEY);
+
+        // Show alert dialog
+        String message = "name: " + name + "\r\nemail: " + email + "\r\nphone: " + phone;
+        new AlertDialog.Builder(this)
+                .setTitle(name)
+                .setMessage(message)
+                .setPositiveButton("ok", null)
+                .show();
+
+        // Show toast message
+        final Toast notifier = Toast.makeText(this, name + ", email: " + email + ", phone: " + phone, Toast.LENGTH_LONG);
+        notifier.show();
+        new CountDownTimer(9000, 1000)
+        {
+            public void onTick(long millisUntilFinished) {notifier.show();}
+            public void onFinish() {notifier.show();
+            }
+        }.start();
+    }
+
+    // LoaderManager.LoaderCallbacks implementation
+    @Override
+    public Loader<Cursor> onCreateLoader(int loaderId, Bundle args) {
+        Uri contactUri = args.getParcelable(CONTACT_URI_KEY);
+        String contactId = contactUri.getLastPathSegment();
+        switch (loaderId) {
+            case CONTACT_LOADER_ID:
+                return new CursorLoader(this, contactUri, new String[] {Contacts.DISPLAY_NAME}, null, null, null);
+            case PHONE_LOADER_ID:
+                return new CursorLoader(this, Phone.CONTENT_URI, new String[] {Phone.NUMBER},
+                        Phone.CONTACT_ID + "=?", new String[] { contactId }, null);
+            case EMAIL_LOADER_ID:
+                return new CursorLoader(this, Email.CONTENT_URI, new String[] {Email.ADDRESS},
+                        Email.CONTACT_ID + "=?", new String[]{contactId}, null);
+            default:
+                // An invalid id was passed in
+                return null;
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Boolean isData = data.moveToFirst();
+        switch (loader.getId()) {
+            case CONTACT_LOADER_ID:
+                if (isData) {
+                    String name = data.getString(data.getColumnIndex(Contacts.DISPLAY_NAME));
+                    contactData.put(NAME_KEY, name);
+                }
+                else
+                    contactData.put(NAME_KEY, "");
+                contactLoaded = true;
+                break;
+            case PHONE_LOADER_ID:
+                if (isData) {
+                    String phone = data.getString(data.getColumnIndex(Phone.NUMBER));
+                    contactData.put(PHONE_KEY, phone);
+                }
+                else
+                    contactData.put(PHONE_KEY, "");
+                phoneLoaded = true;
+                break;
+            case EMAIL_LOADER_ID:
+                if (isData) {
+                    String email = data.getString(data.getColumnIndex(Email.ADDRESS));
+                    contactData.put(EMAIL_KEY, email);
+                }
+                else
+                    contactData.put(EMAIL_KEY, "");
+                emailLoaded = true;
+                break;
+        }
+
+        if (contactLoaded && phoneLoaded && emailLoaded)
+            NotifyUser();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 
 }
